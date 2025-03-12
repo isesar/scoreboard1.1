02d01d9e333ef0fc5d346035eb1f49f742a1c16b
Ivan Sesar <ivan.sesar95@gmail.com>
2025-03-12 18:35:27 +0100

    Add display summary test

--- src/test/java/cli/ScoreboardCLITest.java
+++ src/test/java/cli/ScoreboardCLITest.java
@@ -21,6 +21,7 @@ class ScoreboardCLITest {
     private ScoreboardCLI cli;
     @Mock
     private ScoreboardService service;
+
     @Mock
     private Scanner scanner;
 
@@ -135,4 +136,27 @@ class ScoreboardCLITest {
 //        assertTrue(activeMatches.isEmpty(), "Active matches should be empty after finishing the match.");
 //    }
 
-}
+    @Test
+    void testDisplayLiveSummary_WithMatches() {
+        Team team1 = Team.CANADA;
+        Team team2 = Team.MEXICO;
+        Match match = new Match(team1, team2);
+
+        // Mock service output for match list
+        when(service.getLiveSummary()).thenReturn(List.of(match));
+        when(scanner.nextInt())
+                .thenReturn(4) // User selects display live summary
+                .thenReturn(6); // Exit the CLI
+
+        when(scanner.nextLine()).thenReturn("\n"); // To consume newline characters
+
+        // Act: Run the CLI
+        cli.run();
+
+        // Assert: Verify behavior and output
+        System.out.println("Captured Output: ");
+        assertTrue(outContent.toString().trim().contains("Live Scoreboard:\n" + "1. CANADA 0 - MEXICO 0" ));
+
+    }
+
+    }
